81add64fb5af547940ad591ef5ffe2dd0de9b749
manojathota <163394321+manojathota@users.noreply.github.com>
2026-08-20 08:36:11 +0530

    Fix formatting in Controller.java

--- src/main/java/com/example/Controller.java
+++ src/main/java/com/example/Controller.java
@@ -3,7 +3,7 @@ package com.example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController 
 public class Controller {
 
 	@GetMapping("/hello")
